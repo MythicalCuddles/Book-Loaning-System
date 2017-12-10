0a6207c38c6f9f54fcf93baff987abb2886fb4e5
Melissa <melissabrennan645@gmail.com>
2017-12-10 00:41:45 +0000

    10/12/2017

--- src/Coursework/Controllers/MainMenuController.java
+++ src/Coursework/Controllers/MainMenuController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -17,10 +19,21 @@ import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
     @FXML ImageView ivLogoImage;
+    @FXML Button btnAddBook, btnDisplayBooks, btnDisplayLoans, btnQuit;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ivLogoImage.setImage(new Image("http://astronoteen.org/wp-content/uploads/2015/04/Star-Tau-Ceti-640x120.jpg"));
+        ivLogoImage.setImage(new Image("http://scm.ulster.ac.uk/~B00714027/Files/Software%20Development%20II/CW3/logo.png"));
+
+        btnAddBook.setContentDisplay(ContentDisplay.TOP);
+        btnDisplayBooks.setContentDisplay(ContentDisplay.TOP);
+        btnDisplayLoans.setContentDisplay(ContentDisplay.TOP);
+        btnQuit.setContentDisplay(ContentDisplay.TOP);
+
+        btnAddBook.setGraphic(new ImageView("http://scm.ulster.ac.uk/~B00714027/Files/Software%20Development%20II/CW3/addbook.png"));
+        btnDisplayBooks.setGraphic(new ImageView("http://scm.ulster.ac.uk/~B00714027/Files/Software%20Development%20II/CW3/searchbooks.png"));
+        btnDisplayLoans.setGraphic(new ImageView("http://scm.ulster.ac.uk/~B00714027/Files/Software%20Development%20II/CW3/loanbook.png"));
+        btnQuit.setGraphic(new ImageView("http://scm.ulster.ac.uk/~B00714027/Files/Software%20Development%20II/CW3/exit.png"));
     }
 
     @FXML
